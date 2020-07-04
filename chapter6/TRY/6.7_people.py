10188299b38d76a352880bfe2e05167fb2b50277
MatheGrievink <github@mgrievink.nl>
2020-07-04 17:39:27 +0200

    verder met chapter 6

---
+++ chapter6/TRY/6.7_people.py
@@ -0,0 +1,11 @@
+person = { 
+    'first_name': 'Mathe',
+    'last_name': 'Grievink',
+    'age': '20',
+    'city': 'Dinxperlo',
+}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
