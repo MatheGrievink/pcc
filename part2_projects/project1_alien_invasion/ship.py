687dc6c5bacb145a9d0871259bb60375bbbd2128
MatheGrievink <github@mgrievink.nl>
2020-08-06 15:04:44 +0200

    test

--- part2_projects/project1_alien_invasion/ship.py
+++ part2_projects/project1_alien_invasion/ship.py
@@ -34,6 +34,7 @@ class Ship(Sprite):
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
+
         # Update rect object from self.x
         self.rect.x = self.x
 
